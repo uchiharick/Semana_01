e8ec90f9343631d4e63c3915dee43aaa6ce44c42
Rick-Uchiha <69478369+uchiharick@users.noreply.github.com>
2020-08-17 08:03:44 -0300

    Update 2e.py

--- 2e.py
+++ 2e.py
@@ -1,11 +1,19 @@
-altura = float(input("Altura: "))
-comprimento = float(input("Comprimento: "))
-largura = float(input("Largura: "))
+def calculo():
+    altura = int(input())
+    comprimento = int(input())
+    largura = int(input())
 
-area_piso = largura * comprimento
-volume_da_sala = largura * comprimento * altura
-area_das_paredes_da_sala = (2 * altura) * (largura + 2) * (altura * comprimento)
+    area_piso = largura * comprimento
+    volume_da_sala = largura * comprimento * altura
+    area_das_paredes_da_sala = 2 * altura * (largura + 2) * altura * comprimento
 
-print(f'{area_piso}')
-print(f'{volume_da_sala}')
-print(f'{area_das_paredes_da_sala}')
+    
+    print(area_piso)
+    print(volume_da_sala)
+    print(area_das_paredes_da_sala)
+
+def main():  
+    calculo()
+
+if __name__ == '__main__' :
+    main()
